423e3de4605cc2bbb5fb99483d2417500c60da7d
RodrigoGottschall <ro_criscuolo@hotmail.com>
2024-05-17 16:31:47 -0300

    created index.tsx

---
+++ dashboard/index.tsx
@@ -0,0 +1,13 @@
+import { Flex, Heading } from '@chakra-ui/react';
+import EmployeeTable from './components/EmployeeTable';
+import AddEmployeeButton from './components/AddEmployeeButton';
+
+export default function Home() {
+  return (
+    <Flex direction='column' p={8}>
+      <Heading mb={4}>Dashboard</Heading>
+      <AddEmployeeButton />
+      <EmployeeTable />
+    </Flex>
+  );
+}
